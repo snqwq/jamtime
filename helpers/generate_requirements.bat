0709bbb99fceed2d6f502a3e37ee4b37fc105cc7
snqwq <99054951+snqwq@users.noreply.github.com>
2025-06-29 17:51:50 -0500

    scripts to generate requirements

---
+++ helpers/generate_requirements.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Generating requirements.txt...
+
+pip freeze > requirements.txt
+
+echo Done.
---
+++ helpers/generate_requirements.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo Generating requirements.txt...
+
+pip freeze > requirements.txt
+
+echo Done.
