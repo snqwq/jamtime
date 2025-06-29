482b2910f71f1ffb1b0cb423d02dd32212c8da9e
snqwq <99054951+snqwq@users.noreply.github.com>
2025-06-29 18:50:12 -0500

    make better

--- bot.py
+++ bot.py
@@ -8,6 +8,22 @@ from discord.ext import commands
 bot = discord.Bot()
 
 
+# Commands
+@bot.slash_command(
+    name="Hello",
+    description="Says hello world not much more to it",
+    guild_ids=config.get_guild_ids(),
+)
+async def hello(self, ctx: discord.ApplicationContext):
+    
+        @discord.slash_command(
+        name="hello", description="Make the bot say hello", guild_ids=get_guild_ids()
+    )
+    async def hello(self, ctx: discord.ApplicationContext):
+        """Make the bot say hello to the user (helps check some permissions)"""
+        await ctx.respond(f"Hello {ctx.author}!")
+    
+
 # Startup
 @bot.event
 async def on_ready():
