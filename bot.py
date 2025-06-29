5d1cc6a15ff8579af080124681cd291215a55e56
snqwq <99054951+snqwq@users.noreply.github.com>
2025-06-29 18:43:01 -0500

    config loading and basic bot

--- bot.py
+++ bot.py
@@ -1,2 +1,18 @@
+import config
+
 import discord
 from discord.ext import commands
+
+
+# Init bot
+bot = discord.Bot()
+
+
+# Startup
+@bot.event
+async def on_ready():
+    print(f"bot is logged in as {bot.user.name}#{bot.user.discriminator}")
+
+
+# Running
+bot.run(config.DISCORD_TOKEN)
---
+++ config.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+
+# Variables
+VERSION = "0.0.1"
+IS_DEV = True
+DEV_GUILD_IDS = [1389020690505531422]
+
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Load environment variables into python variables
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+
+# Helper functions
+def get_guild_ids():
+    return DEV_GUILD_IDS if IS_DEV else None
