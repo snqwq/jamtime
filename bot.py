ca3c275de61790b36b6bfc5559ae7921bcf1b243
juuj <jurajhadviger@gmail.com>
2025-06-30 04:45:22 +0200

    start command, actual db entry testing

--- bot.py
+++ bot.py
@@ -1,8 +1,11 @@
 import config
 import time
+import json
+import uuid
 
 import discord
 from discord.ext import commands
+from discord import option
 
 
 # Init bot
@@ -16,7 +19,8 @@ bot = discord.Bot()
     guild_ids=config.get_guild_ids(),
 )
 async def hello(ctx: discord.ApplicationContext):
-    await ctx.respond(f"Hello {ctx.author}!")
+    await ctx.respond(f"Hello {ctx.author.id}!")
+
 
 
 @bot.slash_command(
@@ -26,6 +30,60 @@ async def hello(ctx: discord.ApplicationContext):
 )
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond(f"current time is {time.strftime('%Y-%m-%d %H:%M %Z', time.localtime(time.time()))}, idiot")
+
+
+
+@bot.slash_command(
+    name="start",
+    description="bluh",
+    guild_ids=config.get_guild_ids(),
+)
+@option("name", description="desired name")
+@option("duration", description="desired timer duration")
+async def start(
+   ctx: discord.ApplicationContext,
+   duration: int,
+   name: str
+):
+    unique_id = str(uuid.uuid4())
+
+    await ctx.respond(
+       f"started {name}: {unique_id}, ends at {time.strftime('%Y-%m-%d %H:%M %Z', time.localtime(time.time() + 3600*duration))}"
+    )
+
+
+    # Write new session to db
+
+    #file_path = "database.json"
+    with open("database.json", "a") as f:
+        json.dump(
+            f"{unique_id}",
+            f,
+            indent=4
+        )
+        
+
+# # Append to database
+# file_path = "database.json"
+# with open(file_path, "a") as f:
+#   json.dump(x, f, indent=4)
+
+# with open(file_path) as f:
+#   print(f.read())
+
+
+# x = {
+#     "database": {
+       
+#         "a6066e7a-d5dc-4707-8962-01a572112ce0": {
+#             "start_time": 170000000,
+#             "end_time": 1750000000,
+#             "subscribers": ["bob", "aa"]
+#         }
+#     }
+# }
+
+
 
 
 
---
+++ test.py
@@ -0,0 +1,27 @@
+import json
+import uuid
+
+db_path = "database.json"
+
+# reads data
+with open(db_path, "r") as f:
+    data = json.load(f)
+    print(data)
+    print(type(data))
+
+# # checks if user is in data
+# if user_id not in data['users']:
+#     # checks if data is type list
+#     if type(data) is dict:
+#         data = [data]
+#         print(type(data))
+
+    # updates data with new user
+    data["database"][f"{uuid.uuid4()}"] = {
+        "bob": "AAAAAAAAAAA"
+    }
+    print(data)
+
+    # overwrite the json file with new data
+    with open(db_path, "w") as f:
+        json.dump(data, f, indent=4)
