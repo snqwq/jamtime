2de73ed3509d967cfa819a686685d9302a1c7b2e
juuj <jurajhadviger@gmail.com>
2025-07-01 04:13:44 +0200

    added subscribe, unsubscribe, subscriptions, active_timers, timer_properties and added timer termination upon completion

--- bot.py
+++ bot.py
@@ -46,6 +46,112 @@ async def hello(ctx: discord.ApplicationContext):
 
 
 @bot.slash_command(
+    name="subscriptions",
+    description="bluh",
+    guild_ids=config.get_guild_ids(),
+)
+async def subscriptions(ctx: discord.ApplicationContext):
+
+    user_id = ctx.author.id
+    user_subscriptions = []
+    
+    data = get_db_data()
+    for key in data:
+        entry = data[key]
+        for subscriber in entry["subscribers"]:
+            if subscriber == user_id:
+                user_subscriptions.append(f"{entry["name"]} ({key})")
+                break
+
+    await ctx.respond(f"subscribed to: {user_subscriptions}")
+
+
+@bot.slash_command(
+    name="unsubscribe",
+    description="bluh",
+    guild_ids=config.get_guild_ids(),
+)
+@option("timer_id", description="id of timer to unsubscribe from")
+async def unsubscribe(
+    ctx: discord.ApplicationContext,
+    timer_id: str
+):
+    user_id = ctx.author.id
+    
+    data = get_db_data()
+    for key, value in data.items():
+        print(data)
+        if key == timer_id:
+            value["subscribers"].remove(user_id)
+            write_db_data(data)
+            break
+
+    await ctx.respond(f"unsubscribed from {timer_id}")
+
+
+
+@bot.slash_command(
+    name="subscribe",
+    description="bluh",
+    guild_ids=config.get_guild_ids(),
+)
+@option("timer_id", description="id of timer to subscribe to")
+async def unsubscribe(
+    ctx: discord.ApplicationContext,
+    timer_id: str
+):
+    user_id = ctx.author.id
+    
+    data = get_db_data()
+    for key, value in data.items():
+        print(data)
+        if key == timer_id:
+            value["subscribers"].append(user_id)
+            write_db_data(data)
+            break
+
+    await ctx.respond(f"subscribed to {timer_id}")
+
+
+@bot.slash_command(
+    name="active_timers",
+    description="bluh",
+    guild_ids=config.get_guild_ids(),
+)
+async def active_timers(ctx: discord.ApplicationContext):
+    
+    active_timers = []
+
+    data = get_db_data()
+    for key, value in data.items():
+        active_timers.append(f"{value["name"]} ({key})")
+
+    await ctx.respond(f"active timers: {active_timers}")
+
+
+
+@bot.slash_command(
+    name="timer_properties",
+    description="bluh",
+    guild_ids=config.get_guild_ids(),
+)
+@option("timer_id", description="id of timer to subscribe to")
+async def timer_properties(ctx: discord.ApplicationContext, timer_id: str):
+    
+    properties = None
+
+    data = get_db_data()
+    for key, value in data.items():
+        if key == timer_id:
+            properties = value
+            break
+
+    await ctx.respond(f"timer properties: {properties}")
+
+
+
+
+@bot.slash_command(
     name="start",
     description="bluh",
     guild_ids=config.get_guild_ids(),
@@ -59,40 +165,30 @@ async def start(
 ):
     unique_id = str(uuid.uuid4())
 
-    new_start_time = time.time()
-    new_end_time = new_start_time + 60*duration
+    start_time = time.time()
+    end_time = start_time + 60*duration
 
     await ctx.respond(
-       f"started {name}: {unique_id}, ends at {time.strftime('%Y-%m-%d %H:%M %Z',  time.localtime(new_end_time))}"
+       f"started {name}: {unique_id}, ends at {time.strftime('%Y-%m-%d %H:%M %Z',  time.localtime(end_time))}"
     )
 
     data = get_db_data()
 
     # Updates data with new user
     data[f"{uuid.uuid4()}"] = {
-        "start_time": new_start_time,
-        "end_time": new_end_time,
-        "subscribers": [ctx.author.id]
+        "start_time": start_time,
+        "end_time": end_time,
+        "subscribers": [ctx.author.id],
+        "name": name,
     }
     print(data)
 
     write_db_data(data)
 
-# x = {
-#     "database": {
-       
-#         "a6066e7a-d5dc-4707-8962-01a572112ce0": {
-#             "start_time": 170000000,
-#             "end_time": 1750000000,
-#             "subscribers": ["bob", "aa"]
-#         }
-#     }
-# }
 
 
 
-
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=60)
 async def reminder():
     data = get_db_data()
     
@@ -111,12 +207,11 @@ async def reminder():
 
         # End timer
         if elapsed_time > total_time:
-            message = f"TIMER DONEEEEEEEEEEEEE🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘 {elapsed_time}/{total_time}"
+            message = f"TIMER DONEEEEEEEEEEEEE🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘🦘 {elapsed_time}/{total_time}"
             # Terminate db entry
 
-            new_data = get_db_data()
-            del new_data[key]
-            write_db_data(new_data)
+            del data[key]
+            write_db_data(data)
 
             
         # Half way mark
