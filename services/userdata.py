52c13190c66abb5ec30aad49359cc14f9ab6f177
snqwq <99054951+snqwq@users.noreply.github.com>
2025-07-25 20:33:53 -0500

    minor fix

--- services/userdata.py
+++ services/userdata.py
@@ -59,7 +59,7 @@ def get_user_subscriptions(path: str, user_id: int) -> list:
         entry = data[key]
         for subscriber in entry["subscribers"]:
             if subscriber == user_id:
-                user_subscriptions.append(f"{entry["name"]} ({key})")
+                user_subscriptions.append(f"{entry['name']} ({key})")
                 break
 
     return user_subscriptions
